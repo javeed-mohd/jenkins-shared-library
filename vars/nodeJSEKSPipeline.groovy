bd57d310b1c61f756a1e8c7d2363ee4e51799a6b
Javeed Mohammed <javeedmohd214@gmail.com>
2026-05-09 17:22:35 +0530

    jenkins

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -45,9 +45,16 @@ def call(Map configMap) {
             stage('Unit tests') {
                 steps {
                     script{
-                        sh """
+                        /* sh """
                             npm test
-                        """
+                        """ */
+                        def testResult = sh(script: 'npm test', returnStatus: true)
+                        if (testResult != 0) {
+                            utils.updateCommitStatus('failure', 'Unit tests failed', 'unit-tests')
+                            error "Unit tests failed."
+                        } else {
+                            utils.updateCommitStatus('success', 'Unit tests passed', 'unit-tests')
+                        }
                     }
                 }
             }
